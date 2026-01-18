f337e564985b312fb052d360e1f8d87db4e3c39d
dexterous dawati <dexterousdawati@gmail.com>
2026-01-18 07:54:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then(response=>response.json())
+.then(posts=>{displayPosts(posts)})
+.catch(error=>{console.error("error displaying :",error)});
+
+function displayPosts(posts){
+   const ul= document.getElementById('post-list')
+posts.forEach(post=>{
+    const listItem = document.createElement('li');
+const headerItem = document.createElement('h1');
+const paragraph = document.createElement('p');
+headerItem.textContent = post.title;
+paragraph.textContent = post.body;
+listItem.appendChild(headerItem);
+listItem.appendChild(paragraph);
+ul.appendChild(listItem);
+
+})
+}
